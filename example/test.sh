fa8a64bfeb70d560fac13c804091d539e6a48f84
Karim Alibhai <karim@alibhai.co>
2018-01-05 19:58:21 -0500

    Add nested objects as example

--- example/test.sh
+++ example/test.sh
@@ -4,7 +4,7 @@ alias json="$(dirname $0)/../bin/json"
 jsonFile="$(dirname $0)/test.json"
 
 function at () {
-  echo "$(eval echo "\${${1}_${2}}")"
+  echo "$(eval echo "\${${1}_${2}${3}}")"
 }
 
 ## without root prefix
@@ -26,4 +26,18 @@ echo "array is of size: $(at obj_tags length)"
 
 for ((i=0;i<$(at obj_tags length);++i)); do
   echo "arr[${i}] => $(at obj_tags $i)"
+done
+
+## when array elements are objects
+
+eval $(json < "${jsonFile}")
+
+echo ""
+echo "array of objects is of size: $(at friends length)"
+
+for ((i=0;i<$(at friends length);++i)); do
+  echo "arr[${i}] => {"
+  echo "  id: $(at friends $i _id),"
+  echo "  name: $(at friends $i _name)"
+  echo "}"
 done
